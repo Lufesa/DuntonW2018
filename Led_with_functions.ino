cf4ccd3bc33753d2a9ce04678cc1608bf5820403
Lufesa <luisfsanchez215@gmail.com>
2018-01-12 23:39:47 -0500

    Add files via upload

---
+++ Led_with_functions.ino
@@ -0,0 +1,22 @@
+int led = 9;
+
+void setup() {
+pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+lightUp( led, 500, 5);
+lightUp( led, 250, 5);
+lightUp( led, 100, 25);
+}
+
+void lightUp( int pin, int temps, int maxRep){
+  for (int i=0; i <= maxRep; i++){
+    digitalWrite(pin,HIGH);
+    delay(temps);
+    digitalWrite(pin,LOW);
+    delay(temps);
+  }
+}
+
---
+++ Led_without_functions.ino
@@ -0,0 +1,30 @@
+int led = 9;
+
+void setup() {
+pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+for (int i=0; i <= 5; i++){
+digitalWrite(led,HIGH);
+delay(500);
+digitalWrite(led,LOW);
+delay(500);
+}
+
+for (int i=0; i <= 5; i++){
+digitalWrite(led,HIGH);
+delay(250);
+digitalWrite(led,LOW);
+delay(250);
+}
+
+for (int i=0; i <= 5; i++){
+digitalWrite(led,HIGH);
+delay(100);
+digitalWrite(led,LOW);
+delay(100);
+}
+
+}
